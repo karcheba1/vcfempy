71d4cbb16a1e8ddf8f078a23219cbd6ace35ba9e
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2022-01-22 20:16:08 -0700

    VCFEM-83 VCFEM-84 VCFEM-85 VCFEM-86 VCFEM-87 added tests.py for regression testing

--- src/tests.py
+++ src/tests.py
@@ -5,17 +5,41 @@
 import doctest
 
 import vcfempy.materials as mtl
+import vcfempy.meshgen as msh
+import vcfempy.flow as flw
 
 def main():
+
+    total_tests = 0
+    failed_tests = 0
 
     print('========================================')
     print('Running vcfempy unit tests using doctest')
     print('========================================\n')
 
     print('Testing vcfempy.materials:')
-    print(doctest.testmod(mtl))
+    t = doctest.testmod(mtl)
+    failed_tests += t[0]
+    total_tests += t[1]
+    print(t)
+    print('')
 
-    print('\nTesting complete.\n\n')
+    print('Testing vcfempy.meshgen:')
+    t = doctest.testmod(msh)
+    failed_tests += t[0]
+    total_tests += t[1]
+    print(t)
+    print('')
+
+    print('Testing vcfempy.flow')
+    t = doctest.testmod(flw)
+    failed_tests += t[0]
+    total_tests += t[1]
+    print(t)
+    print('')
+
+    print(f'Testing complete... failed_tests = {failed_tests}, total_tests = {total_tests}')
+    print('')
 
 
 if __name__ == '__main__':
