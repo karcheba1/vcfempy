10638d193fcfddc672d46a7c84ba25d131339c1c
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2022-02-06 11:27:16 -0700

    #2dvv45a #2dvv3y5 #2dvv45d #2dvv3ya bumped to version 0.0.5 and rebuilt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Brandon Karchewski'
 author = 'Brandon Karchewski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
