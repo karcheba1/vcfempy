c0188f5050369ce093418d78699738fbb32e7c85
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2022-02-08 21:54:05 -0700

    #2dvv45a #2dvv3y5 bumped to v0.0.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Brandon Karchewski'
 author = 'Brandon Karchewski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
