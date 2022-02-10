2600cacf24b16f7f32f39c95c88c637eaa82c229
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2022-02-10 16:21:06 -0700

    #2dvv45a #2dvv3y5 bumped to v0.0.9, rebuild, and make html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Brandon Karchewski'
 author = 'Brandon Karchewski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
