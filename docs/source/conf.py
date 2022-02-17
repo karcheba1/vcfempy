f41174ce906bf3121c4a33d825f904c95db0a85f
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2022-02-17 16:23:56 -0700

    #2dvv45d #2dvv3ya #2dvv3y5 merged docs/BoundaryElement2D branch to main and built v0.0.14

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Brandon Karchewski'
 author = 'Brandon Karchewski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.13'
+release = '0.0.14'
 
 
 # -- General configuration ---------------------------------------------------
